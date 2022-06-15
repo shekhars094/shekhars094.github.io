51a6f499ce1e96cd1a8d7adc64b34be1a9a80cda
Shashi Shekhar <shashi@authorcafe.com>
2022-06-15 23:30:33 +0530

    made the changes for transition.

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function transitionEffect() {
   let image = document.querySelector(".logo-image");
   image.src = "./images/shashi_logo_light.svg";
   firstPageContainer.style.backgroundColor = "#000000";
-  firstPageContainer.style.transition = "ease 2s #000000";
+  firstPageContainer.style.transition = "background-color 1000ms linear";
 }
 
-setTimeout(transitionEffect, 2000);
+setTimeout(transitionEffect, 1000);
